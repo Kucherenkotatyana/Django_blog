cda1e3be962f5ab7f1a74abf7a899bd601c2fc48
Tatyana <kucherenkotatyana97@gmail.com>
2023-05-12 14:52:58 +0300

    second commit

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Tatyana',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'May 2, 2023'
-    },
-    {
-        'author': 'Denys',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'May 3, 2023'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
